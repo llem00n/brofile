f9bb2262e562458b97b664be29f39a9031c1c870
llem00n <alex.porubaimikh@gmail.com>
2024-11-20 00:46:16 +0200

    move logs to XDG_STATE_HOME (works on my machine)

--- libbrofile/src/logging.cpp
+++ libbrofile/src/logging.cpp
@@ -2,22 +2,34 @@
 
 #include <spdlog/sinks/rotating_file_sink.h>
 #include <spdlog/spdlog.h>
+
 #include <filesystem>
 
 std::shared_ptr<spdlog::sinks::rotating_file_sink_mt> create_sink() {
-  std::filesystem::path log_file_path;
-  auto home = std::getenv("HOME");
-  if (home != nullptr) {
-    log_file_path = std::filesystem::path(home) / ".brofile";
+  std::filesystem::path log_dir;
+
+  // according to https://specifications.freedesktop.org/basedir-spec/latest/
+  // $XDG_STATE_HOME is a good place to store logs
+  auto xdg_state_home_env = std::getenv("XDG_STATE_HOME");
+  if (xdg_state_home_env != nullptr) {
+    log_dir = std::filesystem::path(xdg_state_home_env);
   } else {
-    log_file_path = std::filesystem::path("~/.brofile");
+    // according to https://wiki.archlinux.org/title/XDG_Base_Directory
+    // $XDG_STATE_HOME should default to $HOME/.local/state should
+    auto home = std::getenv("HOME");
+    if (home != nullptr) {
+      log_dir = std::filesystem::path(home) / ".local" / "state";
+    } else {
+      log_dir = std::filesystem::path("~") / ".local" / "state";
+    }
+  }
+  log_dir = log_dir / "brofile";
+
+  if (!std::filesystem::exists(log_dir)) {
+    std::filesystem::create_directories(log_dir);
   }
 
-  if (!std::filesystem::exists(log_file_path)) {
-    std::filesystem::create_directory(log_file_path);
-  }
-
-  return std::make_shared<spdlog::sinks::rotating_file_sink_mt>(log_file_path / "log.txt", 1048576 * 5, 3);
+  return std::make_shared<spdlog::sinks::rotating_file_sink_mt>(log_dir / "brofile.log", 1048576 * 5, 3);
 }
 
 std::shared_ptr<spdlog::logger> bf::setup_logger(const std::string &name, const std::string &log_level) {
